f7c39800c6b96d553eeaba859e343fea3826260c
Leon simintei <mallfs@icloud.com>
2023-03-19 22:40:47 +0300

    assignment done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def show
+      user = User.find(params[:id])
+      render json: user, include: :items
+    end
 end
